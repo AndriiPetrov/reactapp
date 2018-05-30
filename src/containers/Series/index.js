7d4f21c8eacbf1b55c8e70afcbc021d24e38315e
Bratok3000 <Bratok3000@gmail.com>
2018-05-30 23:39:39 +0300

    almost last commit

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import SeriesList from '../../components/SeriesList';
 
 class Series extends Component {
     state = {
         series: []
     }
 
-    componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+    onSeriesInputChange = e => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
             .then(response => response.json())
-            .then(json => this.setState({ series: json }))
+            .then(json => this.setState({ series: json }));
     }
 
     render() {
         return (
             <div>
                 The lenght of series array - {this.state.series.length}
+                <div>
+                    <input type="text" onChange={this.onSeriesInputChange} />
+                </div>
+                <SeriesList list={this.state.series} />
             </div>
         )
     }
